d98342277aaf6aae784f7afa19dc9f260c49224e
BryanIsraelMazariegosAlarcón <bryanisrael76@yahoo.com>
2025-07-03 16:18:27 -0600

    [ADD]: Initial project structure

---
+++ src/archivo.js
@@ -0,0 +1,14 @@
+console.log("Hola Mundo")
+console.log(5)
+
+//var
+var helloVar = "Hello Var"
+console.log(helloVar)
+
+//let
+let helloLet = "Hello Let"
+console.log(helloLet)
+
+console.log(typeof(helloLet))
+
+
