84ab9c664a1730570901c64197926e09a964c570
BryanIsraelMazariegosAlarcón <bryanisrael76@yahoo.com>
2025-07-04 12:09:39 -0600

    Versión Prueba Rama Feature/API 2

--- src/archivo.ts
+++ src/archivo.ts
@@ -1,17 +1,6 @@
-async function getData() {
-  return Promise.resolve('data');
-}
-
-async function getMoreData(data) {
-  return Promise.resolve(data + 'more data');
-}
-
-async function getAll() {
-  const data = await getData();
-  const moreData = await getMoreData(data);
-  return `All the data: ${data}, ${moreData}`;
-}
-
-getAll().then((all) => {
-  console.log('all the data')
-})
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+eventEmitter.emit('my_event');
+eventEmitter.on('my_event', () => {
+  console.log('data received successfully.');
+});
